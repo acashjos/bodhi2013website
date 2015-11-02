8befc51371ed1fc1879f3298d53ffcf1777422d3
Akash Kurian Jose <acash_92@live.com>
2015-11-02 17:24:18 +0530

    removed unrelated files

--- application/libraries/Bodhi_Form_Validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Bodhi_Form_Validation extends CI_Form_validation {
-function alpha_space($str)
-{
-return ( ! preg_match("/^([a-z ])+$/i", $str)) ? FALSE : TRUE;
-}
-}
--- application/views/admin/footer.php
+++
@@ -1,7 +0,0 @@
-<p style="text-align: center;
-background: #EDEAEA;
-width: 500px;
-margin: auto;
-padding: 10px;">&copy; 2013 Bodhi2k13 | Made with love by Bodhi <a href="http://www.bodhiofficial.in/#webteam" target="_blank">Webteam</a></p>
-</body>
-</html>
